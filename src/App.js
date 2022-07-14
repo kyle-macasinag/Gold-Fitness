2b36c0ef062e89ae5c6bd52683c77e6c41939ef8
kyle.macasinag <kylenmac@verizon.net>
2022-07-14 10:20:59 -0400

    CHORE: Basic component and page files added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import "./App.css"
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import HomePage from "./pages/HomePage";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px">
+      Navbar
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="excercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
   )
 }
 
